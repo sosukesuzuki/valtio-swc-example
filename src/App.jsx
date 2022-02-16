8a6d1bb47cb0f2702397c3116e6df3086948db56
sosukesuzuki <aosukeke@gmail.com>
2022-02-16 16:17:58 +0900

    Introduce valtio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { proxy, useSnapshot } from "valtio";
+
+const state = proxy({ count: 0, text: "hello" });
 
 export default function App() {
-  return <h1>foo</h1>;
+  const snap = useSnapshot(state);
+  return (
+    <div>
+      {snap.count}
+      <button onClick={() => ++state.count}>+1</button>
+    </div>
+  );
 }
